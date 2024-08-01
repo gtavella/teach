5d1b0168e6bda192c178b59e9a0789b9c525969a
Giuseppe Tavella <93888736+gtavella@users.noreply.github.com>
2024-08-02 00:19:00 +0200

    Create lava_mani.py

---
+++ python_decorators/lava_mani.py
@@ -0,0 +1,42 @@
+
+# DECORATORE (QUELLO VERO)
+
+def assicurati_di_lavare_mani1(fai_qualcosa):
+  def nuovo_fai_qualcosa():
+    # prima di fare qualcosa, 
+    # mi lavo le mani 
+    lavati_le_mani()
+    # fai_qualcosa in questo caso e' mangia
+    fai_qualcosa()
+  return nuovo_fai_qualcosa
+    
+
+def mangia1():
+  # visto che ho gia' lavato le mani,
+  # ora posso mangiare come un.. gran signore 
+  chiama_lucia() 
+  facciamo_la_pizza() 
+
+
+mangia1 = assicurati_di_lavare_mani1(mangia1)
+
+
+
+# DECORATORE (ZUCCHERO SINTATTICO)
+
+def assicurati_di_lavare_mani2(fai_qualcosa):
+  def nuovo_fai_qualcosa():
+    # prima di fare qualcosa, 
+    # mi lavo le mani 
+    lavati_le_mani()
+    # fai_qualcosa in questo caso e' mangia
+    fai_qualcosa()
+  return nuovo_fai_qualcosa
+    
+
+@assicurati_di_lavare_mani2
+def mangia2():
+  # visto che ho gia' lavato le mani,
+  # ora posso mangiare come un.. gran signore 
+  chiama_lucia() 
+  facciamo_la_pizza() 
